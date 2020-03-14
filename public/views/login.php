069db815e47ee0819df019c9fa0f9fce1c9fca32
Valentin <valentintean@gmail.com>
2020-03-14 23:52:32 +0200

    Update login.php

--- public/views/login.php
+++ public/views/login.php
@@ -1,34 +1,31 @@
 <?php getHeader($data['pageTitle']); ?>
     <h1><?php echo($data['pageTitle']); ?></h1>
-    <form action="" method="POST" class="tnForm">
-        <div class="tnFormGroup">
+    <form action="" method="POST" class="dv-form">
+        <div class="form-group">
             <label for="username">
                 <i class="fas fa-user"></i>
-                <span> Username</span>
+                <span>Username</span>
             </label>
-            <input type="text" name="username"
-                   class="tnFormInput<?php if (!empty($errors['user_name_error'])): ?> tnInvalidInput<?php endif; ?>"
-                   id="username" value="<?php echo $data['user_name']; ?>"
-                   placeholder="Type here your username">
+            <input type="text" name="username" id="username" class="form-control<?php if (!empty($errors['user_name_error'])): ?> is-invalid<?php endif; ?>">
             <?php if (!empty($errors['user_name_error'])): ?>
-                <span class="tnInvalidInputInfo"><?php echo $errors['user_name_error']; ?></span>
+                <div class="invalid-feedback">
+                    <span><?php echo $errors['user_name_error']; ?></span>
+                </div>
             <?php endif; ?>
         </div>
-        <div class="tnFormGroup">
+        <div class="form-group">
             <label for="password">
                 <i class="fas fa-lock"></i>
-                <span> Password</span>
+                <span>Password</span>
             </label>
-            <input type="password" name="password"
-                   class="tnFormInput<?php if (!empty($errors['user_password_error'])): ?> tnInvalidInput<?php endif; ?>"
-                   id="password" placeholder="Your password goes here">
+            <input type="password" name="password" id="password" class="form-control<?php if (!empty($errors['user_password_error'])): ?> is-invalid<?php endif; ?>">
             <?php if (!empty($errors['user_password_error'])): ?>
-                <span class="tnInvalidInputInfo"><?php echo $errors['user_password_error']; ?></span>
+                <div class="invalid-feedback">
+                    <span><?php echo $errors['user_password_error']; ?></span>
+                </div>
             <?php endif; ?>
         </div>
-        <div class="tnFormSubmit">
-            <button type="submit" class="tnBtn tnSubmitBtn">Submit</button>
-        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
         <p class="tnCentered tnLoginInfo">Don't have an account yet? <a
                     href="<?php echo BASE_URL . '/register/'; ?>">Register</a> now</p>
     </form>
