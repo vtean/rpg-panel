5ab5ccdfa758af8acfc31adb2479474bead788b6
Gabriel B <gabrielbaraboi5@gmail.com>
2020-03-15 00:31:26 +0200

    Login design.

---
+++ public/resources/js/scripts.js
@@ -0,0 +1,14 @@
+(function ($) {
+    "use strict"; // Start of use strict
+
+    $('.login-form').each(function () {
+        $(this).on('blur', function () {
+            if ($(this).val().trim() !== "") {
+                $(this).addClass('login-focused');
+            } else {
+                $(this).removeClass('login-focused');
+            }
+        })
+    })
+
+})(jQuery);
