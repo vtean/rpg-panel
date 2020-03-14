d433f5205fad534f5fa94dac35c3fcc11c81800e
Gabriel B <gabrielbaraboi5@gmail.com>
2020-03-14 21:38:52 +0200

    Added login system.

--- config.php
+++ config.php
@@ -7,22 +7,22 @@
  */
 
 // define the database host
-define('DB_HOST', '');
+define('DB_HOST', 'localhost');
 
 // define the database name
-define('DB_NAME', '');
+define('DB_NAME', 'panel');
 
 // define the database password
 define('DB_PASS', '');
 
 // define the database user
-define('DB_USER', '');
+define('DB_USER', 'root');
 
 // define the root path
 define('ROOT_PATH', dirname(__FILE__));
 
 // define the base url
-define('BASE_URL', '');
+define('BASE_URL', 'http://panel.loc/');
 
 // define the site name
 define('SITE_NAME', 'RPG Panel');
