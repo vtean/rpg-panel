ab2dcf88e0acebb96b3b063811f75831307a9603
Valentin <valentintean@gmail.com>
2020-03-15 00:43:45 +0200

    Update dreamvibe.css, footer.php, main.php

--- public/views/layouts/footer.php
+++ public/views/layouts/footer.php
@@ -1,3 +1,9 @@
+    <div class="dv-footer">
+        <div class="dv-copyright">
+            <p>Developed with <i class="fas fa-heart"></i> and a lot of <i class="fas fa-coffee"></i> by <a href="">Lust</a> and <a href="">Indigo</a></p>
+            <p>© DreamVibe Community 2020</p>
+        </div>
+    </div>
     </div>
     </main>
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
--- public/views/main.php
+++ public/views/main.php
@@ -1,3 +1,33 @@
 <?php getHeader($data['pageTitle']); ?>
     <?php flashMessage(); ?>
+    <div class="dv-widget">
+        <h4 class="dv-page-title">Server stats</h4>
+        <div class="dv-server-stats clearfix">
+            <div class="dv-server-stats-item">
+                <i class="fas fa-users"></i>
+                <span class="dv-first">126</span>
+                <span class="dv-second">Online Players</span>
+            </div>
+            <div class="dv-server-stats-item">
+                <i class="fas fa-user-friends"></i>
+                <span class="dv-first">256</span>
+                <span class="dv-second">Registered Users</span>
+            </div>
+            <div class="dv-server-stats-item">
+                <i class="fas fa-home"></i>
+                <span class="dv-first">860</span>
+                <span class="dv-second">Houses</span>
+            </div>
+            <div class="dv-server-stats-item">
+                <i class="fas fa-building"></i>
+                <span class="dv-first">183</span>
+                <span class="dv-second">Businesses</span>
+            </div>
+            <div class="dv-server-stats-item">
+                <i class="fas fa-car-side"></i>
+                <span class="dv-first">690</span>
+                <span class="dv-second">Personal Vehicles</span>
+            </div>
+        </div>
+    </div>
 <?php getFooter(); ?>
