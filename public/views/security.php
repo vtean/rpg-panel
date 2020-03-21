06c2b547b377cbd39bd2191157b82627eefab6d2
Gabriel B <gabrielbaraboi5@gmail.com>
2020-03-22 01:53:22 +0200

    2FA auth check on login, and a million dead neurons

---
+++ public/views/security.php
@@ -0,0 +1,24 @@
+<?php getHeader($data); ?>
+    <div class="card col-md-5 login">
+        <div class="card-body">
+            <span class="login-icon">
+                <i class="fas fa-shield-alt"></i>
+            </span>
+            <h2 class="login-title"><?php echo $data['lang']['login_txt']; ?></h2>
+            <form action="" method="POST" class="dv-form">
+                <div class="form-group login-group">
+                    <input type="text" name="secret" id="secret"
+                           class="login-form<?php if (!empty($errors['secret_error'])): ?> is-invalid<?php endif; ?>">
+                    <span class="login-span secret"></span>
+                    <?php if (!empty($errors['secret_error'])): ?>
+                        <div class="invalid-feedback">
+                            <span><?php echo $errors['secret_error']; ?></span>
+                        </div>
+                    <?php endif; ?>
+                </div>
+                <p class="text-align-center"><?php echo $data['lang']['lost_secret_txt']; ?> <a href=""><?php echo $data['lang']['here_txt']; ?></a></p>
+                <button type="submit" class="login-btn" name="authCheck"><?php echo $data['lang']['login_txt']; ?></button>
+            </form>
+        </div>
+    </div>
+<?php getFooter(); ?>
--- system/Models/General.php
+++ system/Models/General.php
@@ -17,7 +17,12 @@ class General
 
     public function checkFullAccess($username)
     {
-        $sql = "SELECT FullDostup1 FROM sv_fulldostup WHERE FullDostup1=:username";
+        $sql = "SELECT * FROM sv_fulldostup WHERE FullDostup1=:username OR FullDostup2=:username 
+                                            OR FullDostup3=:username OR FullDostup4=:username 
+                                            OR FullDostup5=:username OR FullDostup6=:username 
+                                            OR FullDostup7=:username OR FullDostup8=:username 
+                                            OR FullDostup9=:username OR FullDostup10=:username 
+                                            OR FullDostup11=:username OR FullDostup12=:username";
         // prepare the query
         $this->db->prepareQuery($sql);
         // bind params
