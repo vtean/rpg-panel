ade2637edab2a491c5d6c8c4530cdc8e46b5b6b8
Valentin <valentintean@gmail.com>
2020-03-24 00:30:19 +0200

    PUSH^2

--- system/Controllers/SecurityController.php
+++ system/Controllers/SecurityController.php
@@ -44,9 +44,10 @@ class SecurityController extends Controller
                     $sendCode = $_SESSION['sec_code'];
                     $message = "A fost depistata o autentificare pe contul tau de pe un nou IP. <br>
                             Daca ai fost tu, introdu pe server codul de mai jos. In cazul in care nu tu te-ai conectat, iti recomandam sa iti schimbi parola. <br><br>    
-                            Codul de securitate pentru contul tau este: $sendCode.";
-                if (!isset($_POST['authCheck'])){
-                    sendMail($mail, 'do-not-reply@dreamvibe.ro', 'DreamVibe RPG', 'Confirmare Login', $message);
+                            Codul de securitate pentru contul tau este: " . $sendCode . ".";
+                    if (!isset($_POST['authCheck'])) {
+                        sendMail($mail, 'do-not-reply@dreamvibe.ro', 'DreamVibe RPG', 'Confirmare Login', $message);
+                    }
                 }
             }
             if ($_SERVER['REQUEST_METHOD'] == 'POST') {
